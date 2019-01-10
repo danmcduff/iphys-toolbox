830eb72403d6525e8532a7c34ca0e43e3f497ab7
ethanatball <ethanatball@users.noreply.github.com>
2019-01-10 15:32:39 -0500

    Added erractic feature point removal, loading sample data, and fixed typos

--- BCG_BALAKRISHNAN.m
+++ BCG_BALAKRISHNAN.m
@@ -11,8 +11,8 @@ function [BCG, PR, HR_ECG, PR_PPG, SNR] = BCG_BALAKRISHNAN(VideoFile, FS, StartT
 %       PlotTF                  = Boolean to turn plotting results on or off.
 %
 %   Outputs:
-%       BVP                     = Processed Blood Volume Pulse (BVP).
-%       PR                      = Estimated Pulse Rate (PR) from processed BVP timeseries using peak in periodogram.
+%       BCG                     = Processed Ballistocardiogram (BCG).
+%       PR                      = Estimated Pulse Rate (PR) from processed BCG timeseries using peak in periodogram.
 %       HR_ECG                  = Gold standard Heart Rate (HR) measured from the ECG timeseries R-waves for the window.
 %       PR_PPG                  = Pulse Rate (PR) measured from the PPG timeseries systolic onsets for the window.
 %       SNR                     = Signal-to-Noise Ratio (SNR) calculated based on the ECG HR frequency using a method adapted from the method by G. de Haan, TBME, 2013.
@@ -52,45 +52,69 @@ BBox = [1,1,VidObj.width,VidObj.height];
 
 MinQ = 0.01;
 VidFrame = readFrame(VidObj);
-Points0 = detectMinEigenFeatures(rgb2gray(VidFrame),'ROI',BBox,'MinQuality',MinQ);
 
-if length(Points0) < 5
-    BCG=NaN; PR=NaN; HR_ECG=NaN; PR_PPG=NaN; SNR=NaN;
-    return
-end
-
-tracker = vision.PointTracker;
-initialize(tracker,Points0.Location,VidFrame);
-
-%% Read Video and Spatially Average:
-T = zeros(FramesToRead-1,1);%Initialize time vector
-D = zeros(FramesToRead-1,size(Points0.Location,1));%initialize D
-FN = 0;
-while hasFrame(VidObj) && (VidObj.CurrentTime <= StartTime+Duration)
-    FN = FN+1;
-    T(FN) = VidObj.CurrentTime;
-    VidFrame = readFrame(VidObj);
+try%allow to run example video file without Computer Vision Toolbox by loading tracking results in catch
+    Points0 = detectMinEigenFeatures(rgb2gray(VidFrame),'ROI',BBox,'MinQuality',MinQ);
     
-    %position for optional face detection/tracking - originally specified in reference as using Viola Jones, 2004 to isolate face and region including the eyes
     
-    [Points, Validity] = step(tracker,VidFrame);
-    D(FN,:) = Points(:,2);%y, vertical component
-end
-%originally specified in reference with cubic-spine interpolation to 250Hz (from 30 Hz).
+    if length(Points0) < 5
+        BCG=NaN; PR=NaN; HR_ECG=NaN; PR_PPG=NaN; SNR=NaN;
+        return
+    end
+    
+    tracker = vision.PointTracker;
+    initialize(tracker,Points0.Location,VidFrame);
+    
+    %% Read Video and Spatially Average:
+    T = zeros(FramesToRead-1,1);%Initialize time vector
+    Y = zeros(FramesToRead-1,size(Points0.Location,1));%initialize Y
+    FN = 0;
+    while hasFrame(VidObj) && (VidObj.CurrentTime <= StartTime+Duration)
+        FN = FN+1;
+        T(FN) = VidObj.CurrentTime;
+        VidFrame = readFrame(VidObj);
+        
+        %position for optional face detection/tracking - originally specified in reference as using Viola Jones, 2004 to isolate face rectangle and exclude region including the eyes
+        
+        [Points, Validity] = step(tracker,VidFrame);
+        Y(FN,:) = Points(:,2);%y, vertical component
+    end
+    
+catch TrackError%optional catch to run example without Computer Vision Toolbox by loading tracking results in catch
+    if(strcmp(TrackError.message,'Undefined function ''detectMinEigenFeatures'' for input arguments of type ''uint8''.'))
+        [VideoFilePath,VideoFileName,VideoFileExt] = fileparts(VideoFile);
+        if(strcmp([VideoFileName VideoFileExt],'video_example.mp4'))%only run for example video
+            if(exist(VideoFile,'file'))%only allow to proceed for the example video where we have the results of the point tracking
+                fprintf('Tracking could not be completed without the Computer Vision Toolbox.\nLoading previously run tracking results for ''video_example.mp4''.\n')
+                load([VideoFilePath '\BCGTracking.mat']);%contains Y and T results for video_example.mp4 run with defaults FS=30, StartTime=0, and Duration=60
+            end
+        else%show error - can only avoid tool box requirement to run video_example with loaded tracking results
+            TrackError
+        end
+    else%some other error
+        TrackError
+    end
+end% end try-catch
+%originally specified in reference with cubic-spine interpolation to 250Hz (from 30 Hz) to match sampling rate of ECG device.
 
 %% Signal Processing:
+%Remove erratic feature points - points whose max movement between consecutive frames exceeds the mode of the distribution of max motions
+MaxYMotions=max(floor(diff(Y,1,2)));
+UnstableMask=MaxYMotions>mode(MaxYMotions);
+YS=Y(:,~UnstableMask);
+
 NyquistF = 1/2*FS;
 [B,A] = butter(3,[LPF/NyquistF HPF/NyquistF]);%5th order butterworth filter in reference
-D_Filt = filtfilt(B,A,double(D));
+Y_Filt = filtfilt(B,A,double(YS));
 
-D_Filt2 = bsxfun(@minus, D_Filt, D_Filt(1,:));
-DL2 = sqrt(sum(D_Filt2.^2,2));
-DMask = DL2<=quantile(DL2,.75);
+Y_Filt2 = bsxfun(@minus, Y_Filt, Y_Filt(1,:));
+YL2 = sqrt(sum(Y_Filt2.^2,2));
+YMask = YL2<=quantile(YL2,.75);
 
-D_Filt3=D_Filt2(DMask,:);
-[Coeff, ~, Latent] = pca(D_Filt3);
+Y_Filt3=Y_Filt2(YMask,:);
+[Coeff, ~, Latent] = pca(Y_Filt3);
 
-Score = bsxfun(@minus, D_Filt, mean(D_Filt))/Coeff';
+Score = bsxfun(@minus, Y_Filt, mean(Y_Filt))/Coeff';
 
 %% Component Selection
 F=find(T>0);
@@ -110,7 +134,7 @@ PR = FRange(argmax(Pxx(FMask,NumPC),1))*60;
 
 BCG_F = Score(:,NumPC);
 
-BCG_N = BCG_F-mean(BCG_F); 
+BCG_N = BCG_F-mean(BCG_F);
 BCG = BCG_N;
 
 %% Ground Truth HR:
@@ -139,7 +163,7 @@ SNR = bvpsnr(BCG,FS,HR_ECG,PlotSNR);
 
 %% Optionally Plot Timeseries
 if(PlotTF)
-    %Plot ECG, PPG, iPPG timeseries    
+    %Plot ECG, PPG, iBCG timeseries
     figure
     
     if ~isempty(PPGFile)
@@ -149,7 +173,7 @@ if(PlotTF)
         hold on
         plot(ECG.peaks(ECGPeakMask)/ECG.fs,ECG.data(ECG.peaks(ECGPeakMask)),'*')
         ylabel('ECG (a.u.)')
-        title('BCG Method - ECG, PPG, iPPG Timeseries')
+        title('BCG Method - ECG, PPG, iBCG Timeseries')
         
         %Plot PPG
         Ax2=subplot(3,1,2);
@@ -158,12 +182,12 @@ if(PlotTF)
         plot(PPG.peaks(PPGPeakMask)/PPG.fs,PPG.data(PPG.peaks(PPGPeakMask)),'*')
         ylabel('PPG (a.u.)')
         
-        %Plot iPPG
+        %Plot iBCG
         Ax3=subplot(3,1,3);
-        plot(T,BVP)
+        plot(T,BCG)
         hold on
-        ylabel('iPPG (a.u.)')
-
+        ylabel('iBCG (a.u.)')
+        
         xlabel('Time (s)')
         
         linkaxes([Ax1,Ax2,Ax3],'x')
@@ -174,21 +198,21 @@ if(PlotTF)
         hold on
         plot(ECG.peaks(ECGPeakMask)/ECG.fs,ECG.data(ECG.peaks(ECGPeakMask)),'*')
         ylabel('ECG (a.u.)')
-        title('BCG Method - ECG, iPPG Timeseries')
+        title('BCG Method - ECG, iBCG Timeseries')
         
         %Plot iPPG
         Ax2=subplot(2,1,2);
-        plot(T,BVP)      
+        plot(T,BCG)
         hold on
-        ylabel('iPPG (a.u.)')
-
+        ylabel('iBCG (a.u.)')
+        
         xlabel('Time (s)')
         
         linkaxes([Ax1,Ax2],'x')
     end
     
     xlim([StartTime StartTime+Duration])
-
+    
 end%endif plot
 
 %% Remove Backup Functions
